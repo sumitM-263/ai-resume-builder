11e68fcb007a17dd0b57b21cffa6fd4824b911fe
Sumit Maurya <hsumit263@gmail.com>
2026-05-18 12:38:34 +0530

    added footer section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../components/home/Hero';
 import Features from '../components/home/Features';
 import Testimonial from '../components/home/Testimonial';
 import CallToAction from '../components/home/CallToAction';
+import Footer from '../components/home/Footer';
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Features />
       <Testimonial />
       <CallToAction />
+      <Footer />
     </div>
   );
 };
