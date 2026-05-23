27d4a89b99626173ab99ec92cc01d6a3f412b078
Sumit Maurya <hsumit263@gmail.com>
2026-05-23 12:09:46 +0530

    skills form added in resume builder

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -10,6 +10,7 @@ import ProfessionSummaryForm from '../components/ProfessionalSummaryForm';
 import ExperienceForm from '../components/ExperienceForm';
 import EducationForm from '../components/EducationForm';
 import ProjectForm from '../components/ProjectForm';
+import SkillsForm from '../components/SkillsForm';
 
 
 const ResumeBuilder = () => {
@@ -129,7 +130,18 @@ const ResumeBuilder = () => {
                   )
                 }
 
+                {
+                  activeSection.id === 'skills' && (
+                    <SkillsForm data={resumeData.skills} onChange={(data) => setResumeData(prev => ({ ...prev, skills: data }))} />
+                  )
+                }
+
               </div>
+
+                <button className='bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                  Save Changes
+                </button>
+
             </div>
           </div>
 
