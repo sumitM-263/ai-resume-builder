5d536dfc18bbc2746c1f7330dbfe8191effc7e0b
Sumit Maurya <hsumit263@gmail.com>
2026-06-28 14:12:39 +0530

    restructured server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoutes.js';
 import resumeRouter from './routes/resumeRoutes.js';
@@ -10,18 +11,34 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// database connection
-await connectDB();
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.send("Server is live..."))
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is live...');
+});
 
 app.use('/api/users', userRouter);
 app.use('/api/resumes', resumeRouter);
 app.use('/api/ai', aiRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+
+// Start Server
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
